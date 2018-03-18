02b3c409d9e6dd69905839115b2a9ce79d829a4d
Aiden Gilmartin <21668472+aidengilmartin@users.noreply.github.com>
2018-03-18 22:53:34 +0000

    Update themeSwitch.js
    Added some comments

--- js/themeSwitch.js
+++ js/themeSwitch.js
@@ -1,4 +1,4 @@
 function themeSwitch(name) {
-    document.getElementById('style').setAttribute('href', 'css/' + name + '.css');
-    localStorage.setItem('theme', name);
+    document.getElementById('style').setAttribute('href', 'css/' + name + '.css'); // Set the HTML Link tag to the correct href
+    localStorage.setItem('theme', name); // Store the choice in localstorage so that it is remembered
 }
